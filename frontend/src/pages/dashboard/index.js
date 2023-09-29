d584b28ff89ca0f355b3eb88a449adbbde2db1ae
thebriansiu <thebriansiu@gmail.com>
2023-09-29 15:39:41 -0700

    fix in index.js

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,6 +1,5 @@
 export * from "@/pages/dashboard/home";
 export * from "@/pages/dashboard/users/users";
-export * from "@/pages/dashboard/customers/";
 export * from "@/pages/dashboard/projects/projects";
 export * from "@/pages/dashboard/notifications";
 export * from "@/pages/dashboard/profile";
