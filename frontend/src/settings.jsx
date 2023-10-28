4390690652e61c19d7df60ee31bca8f8b7681a9e
thebriansiu <thebriansiu@gmail.com>
2023-10-27 22:40:13 -0700

    auth0 insert user, delete, and insert to DB and removing hashed password

--- frontend/src/settings.jsx
+++ frontend/src/settings.jsx
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://scuplin-crm-365085825c4c.herokuapp.com';
-export const API_URL = "http://localhost:3000";
+export const API_URL = 'https://scuplin-crm-365085825c4c.herokuapp.com';
+// export const API_URL = "http://localhost:3000";
