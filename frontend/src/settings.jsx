0cd6446480ac4cbcca8c98701533cd2e7cdfa9fa
thebriansiu <thebriansiu@gmail.com>
2023-12-12 20:51:30 -0800

    client id testing

--- frontend/src/settings.jsx
+++ frontend/src/settings.jsx
@@ -1,7 +1,7 @@
 export const domain = "dev-8dixmhiwz587kgpl.us.auth0.com";
-export const clientId = "15lDMYMAdGAyqLxUhX5tPjWaLJAI2y1q";
+// export const clientId = "15lDMYMAdGAyqLxUhX5tPjWaLJAI2y1q";
 export const API_URL = 'https://scuplin-crm-365085825c4c.herokuapp.com';
 // export const API_URL = "http://localhost:3000";
 // export const domain = "dev-8dixmhiwz587kgpl.us.auth0.com";
-// export const clientId = "8ujHhQ54sLfQjsSRiZNacRFdXSZAeH7p";
+export const clientId = "8ujHhQ54sLfQjsSRiZNacRFdXSZAeH7p";
 
