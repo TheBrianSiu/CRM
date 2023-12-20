8136ff3991e32e4ece66d0cd1055c29cddd3339a
thebriansiu <thebriansiu@gmail.com>
2023-12-19 18:12:39 -0800

    remove local backend

--- frontend/src/settings.jsx
+++ frontend/src/settings.jsx
@@ -1,5 +1,5 @@
 export const domain = 'dev-8dixmhiwz587kgpl.us.auth0.com';
-// export const clientId = "15lDMYMAdGAyqLxUhX5tPjWaLJAI2y1q";
-// export const API_URL = 'https://scuplin-crm-365085825c4c.herokuapp.com';
-export const API_URL = 'http://localhost:3000';
-export const clientId = '8ujHhQ54sLfQjsSRiZNacRFdXSZAeH7p';
+export const clientId = "15lDMYMAdGAyqLxUhX5tPjWaLJAI2y1q";
+export const API_URL = 'https://scuplin-crm-365085825c4c.herokuapp.com';
+// export const API_URL = 'http://localhost:3000';
+// export const clientId = '8ujHhQ54sLfQjsSRiZNacRFdXSZAeH7p';
