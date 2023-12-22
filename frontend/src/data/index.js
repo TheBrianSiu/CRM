260f0367458f05ac9b9d0a3a9171f1f9ad276c26
thebriansiu <thebriansiu@gmail.com>
2023-12-21 21:07:52 -0800

    recommit for netlify

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -4,5 +4,5 @@ export * from '@/data/projects-table-data';
 export * from '@/data/customers-data';
 export * from '@/data/users-data';
 export * from '@/data/projects-data';
-export * from '@/data/menu-options-Data';
+export * from '@/data/menu-options-data';
 export * from '@/data/auth-data';
